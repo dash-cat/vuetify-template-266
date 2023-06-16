bbb74d23f150f1e73afe5e71e2b00dd923d1c24b
sudgr <122168129+sudgr@users.noreply.github.com>
2023-06-16 18:50:35 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <v-app>
+    <Weather/>
+   </v-app>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+export default {
+  components: { Weather },
+   name: 'App',
+   data: () => ({
+      //
+   })
+}
+</script>
